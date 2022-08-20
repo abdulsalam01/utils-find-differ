6181e9f202eb416724c4158e43444d375f1a1a35
Abdul Salam <abdulsalam121196@gmail.com>
2022-08-20 09:32:52 +0700

    ADD: guide

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import pandas as pd
 # with different size / total data of files/datasets
 # make sure: use fileNew as the data that has many data than file parameter
 # len(fileNew) > len(file)
+# column need to be exists in fileNew data
+# idx has to be number
 def see_differ(file, fileNew, column, idx):
     sheet1, sheet2 = None, None
     with file as reader1, fileNew as reader2:
@@ -31,5 +33,5 @@ fileNew = pd.ExcelFile(path_excel + "FINAL_DPL_KM4.xlsx")
 # check what column that we want to be see the differ
 # at which index the file parameter (1st parameter) has the same value with column parameter
 diff_sd = see_differ(file=file, fileNew=fileNew, column="NIDN", idx=4)
-# export the difference
+# export the difference to excel
 diff_sd.to_excel(path_excel + 'residue_new_dosen_from_output_july_km4.xlsx')
